4c7550a9b93d5553cd21ca39709636f045e2585b
saikiran0018 <109685878+saikiran0018@users.noreply.github.com>
2022-07-29 13:21:11 +0530

    Add files via upload

---
+++ veriable name.py
@@ -0,0 +1,35 @@
+print('cb'.isidentifier())               #true
+print('_ui'.isidentifier())              #true
+print('_e '.isidentifier())              #false
+
+
+print('z@s'.isidentifier())              #false
+print('_1'.isidentifier())               #true
+
+
+print('1_1_2'.isidentifier())            #false
+print('_4a_vv'.isidentifier())           #true
+
+
+
+x='python'
+print(x.upper())                           #PYTHON
+
+
+
+
+x='python'
+print(x.isupper())                          #false
+
+
+x='python'
+print(x.islower())                             #true
+
+
+a=6
+print(a)                                       #6
+a=4
+print(a)                                       #4
+
+
+
